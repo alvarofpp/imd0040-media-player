090c020b2d7ed73670b0925f859b138f93f404b1
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-06-23 21:00:23 -0300

    Comentários

--- src/ufrn/alvarofpp/exceptions/FieldNotFoundException.java
+++ src/ufrn/alvarofpp/exceptions/FieldNotFoundException.java
@@ -1,5 +1,8 @@
 package ufrn.alvarofpp.exceptions;
 
+/**
+ * Essa exceção é ativada quando um fos campos do formulário está vazio.
+ */
 public class FieldNotFoundException extends Exception {
 
     public FieldNotFoundException(String field) {
--- src/ufrn/alvarofpp/exceptions/UserExistException.java
+++ src/ufrn/alvarofpp/exceptions/UserExistException.java
@@ -1,5 +1,8 @@
 package ufrn.alvarofpp.exceptions;
 
+/**
+ * Essa exceção é ativada quando o usuário que está tentando ser registrado já existe.
+ */
 public class UserExistException extends Exception {
 
     public UserExistException(String username) {
